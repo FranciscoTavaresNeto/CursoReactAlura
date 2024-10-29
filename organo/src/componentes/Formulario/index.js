169dd03c8343f28487d344b5e7ed6bf6ee6eecee
Francisco Tavares <f.tavares928@gmail.com>
2024-10-29 04:06:10 -0300

    Trabalhando com props

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,14 +1,34 @@
 import './Formulario.css'
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botao'
 
 const Formulario = () => {
+
+    const times = [
+        'Programacao',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovacao e Gestao'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form Enviado!')
+    }
+
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
-                <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>    
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o seu nome"/>
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o seu cargo"/>
                 <CampoTexto label="Imagem" placeholder="Informe o local da imagem"/>
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+                <Botao>Criar Card</Botao>
             </form>
         </section>
     )
